c1697f2ae2a9d195a7a9c8234921f542e6e83b4f
Raushank7667 <rau7633020372@gmail.com>
2026-06-28 18:29:47 +0530

    userspace programm added

---
+++ 02_dev_nr/test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main(){
+
+	int ret = open("/dev/mydevice", O_RDONLY);
+	if(ret == -1){
+            printf("Opening was not possible\n");
+	    return -1;
+
+	}
+	printf("Opening was successfull\n");
+	close(ret);	
+ return 0;
+
+}
